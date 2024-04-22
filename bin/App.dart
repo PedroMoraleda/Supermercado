aa6250812b269125c0c829f8aa87fd6d8a321f7b
Pedro <pedromoramar3@gmail.com>
2024-04-22 18:41:03 +0200

    felipe

--- bin/App.dart
+++ bin/App.dart
@@ -7,9 +7,16 @@ static  inicioApp() async {
     int? seleccion = pedirOpcion();
     switch (seleccion) {
       case 1:
-        catalogo();
+        busqueda();
         break;
-
+      case 2:
+        lista();
+        break;
+      case 3:
+        ofertas();
+        break;
+      case 4:
+        carrito();
       default:
         inicioApp();
     }
@@ -21,20 +28,45 @@ static  inicioApp() async {
     int? opcion;
     do {
       stdout.writeln('''Selecciona una de las siguientes opciones:
-        1 - Catalogo de productos
-        2 - Busqueda de productos
-        3 - Lista de la compra
-        4 - Ofertas
-        5 - Carrito de la compra''');
+        1 - Busqueda de productos
+        2 - Lista de la compra
+        3 - Ofertas
+        4 - Carrito de la compra''');
       opcion = int.tryParse(stdin.readLineSync() ?? 'e');
     } while (opcion == null);
     return opcion; }
 
- static String pedirNombre() {
+ static String pedirProducto() {
     stdout.writeln("Escribe el nombre del producto a consultar");
+    return stdin.readLineSync() ?? "error";//falta
+}
+  static String listaCompra() {
+    stdout.writeln("Escribe el nombre del producto que quieres añadir a la lista");
     return stdin.readLineSync() ?? "error";
-  }
-  static  catalogo() async {
-    String respuesta = pedirNombre();
+}
+  static String mostrarOferta() {
+    stdout.writeln("¡Aun no hay ofertas!");
+    return stdin.readLineSync() ?? "error";// mal
+}
+  static String consultarCarrito() {
+    stdout.writeln("Tu carrito esta vacio");
+    return stdin.readLineSync() ?? "error"; // mal
+}
+
+  static  busqueda() async {
+    String respuesta = pedirProducto();
       }
+
+  static  lista() async {
+    String respuesta = listaCompra();
+      }
+
+  static  ofertas() async {
+    String respuesta = mostrarOferta();
+      }
+
+  static  carrito() async {
+    String respuesta = consultarCarrito();
+      }
+
 }
